730acd4b2fb2d17eb93c587823a06141927a5216
Igor <greenforgit@gmail.com>
2019-10-18 16:32:15 +0300

    edit

--- Frostible/website/js/main.js
+++ Frostible/website/js/main.js
@@ -1,7 +1,14 @@
 let menuTogle = document.querySelector('.toggle-button');
+let searchButton = document.querySelector('.search')
+let searchField = document.querySelector('.header__search-field')
 
 menuTogle.addEventListener('click', function () {
     this.classList.toggle('active');
     document.querySelector('.nav').classList.toggle('opened');
     document.querySelector('body').classList.toggle('locked');
 })
+
+searchButton.addEventListener('click', function () {
+    searchField.classList.toggle('active');
+})
+
