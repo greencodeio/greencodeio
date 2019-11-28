ae342a3fe835bd7c81e8b06fc49dd2e244058579
Igor <greenforgit@gmail.com>
2019-11-28 15:23:58 +0200

    change

--- kangen/js/show-more.js
+++ kangen/js/show-more.js
@@ -1,6 +1,5 @@
 let dots = document.querySelector('.dots');
 let more = document.querySelector('.more');
-let nav__list = document.querySelector('.nav__list');
 
 let btn = document.querySelector('.buy-water__description_button').addEventListener('click', function () {
     dots.style.display = 'none';
